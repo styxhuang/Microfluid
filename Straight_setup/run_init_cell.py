33f0ef20aa1a171883bc2b289bb48fb52917e3b3
Ming Huang <huanghun@umich.edu>
2016-08-25 00:21:19 +0000

    run_init_cell.py created online with Bitbucket

---
+++ Straight_setup/run_init_cell.py
@@ -0,0 +1,355 @@
+# /usr/bin/env hoomd
+from hoomd_script import *
+
+import os
+import shutil
+import re
+import sys
+import itertools
+import xml.etree.ElementTree as ET
+import math as m
+import numpy as np
+
+######################
+##### Init params #####
+######################
+
+# free parameters, units
+# of particle diameter(1.0)
+N_cell = 1 #number of cells, only 0 or 1
+lz = 20.0
+box_len = lx = ly = 15.0
+mass = 0.10
+
+# dumps
+dump_period = 1e2
+rsz_steps = 1e3
+eql_steps = 1e3
+
+# geometry
+wall_vf = 61.35
+#wall_vf = 6.135
+solv_vf = 4.0
+
+# lattice const determined by wall_vf
+a = ((2./3.*m.pi)/wall_vf)**(1./3.)
+
+# solve for the N_solvent from given volume fraction
+N_solv = int(6/m.pi*solv_vf*lz*ly*lx)
+
+# get the job index from PBS_ARRAYID, or return 0 if it is not specified (for test jobs)
+def get_array_id():
+    pbs_arrayid = os.getenv('PBS_ARRAYID');
+    if pbs_arrayid is None:
+        return 0
+    else:
+        return int(pbs_arrayid) - 1;
+
+# set up the initial and final box dimensions
+# beads are all diameter = 1.0
+id = get_array_id();
+fName = '-eql-' + str(int(id) + 1)
+
+# random solvent **placer**, no system init via hoomd
+def random_solv_plcr():
+    # from lx,ly,lz randomly pluck positions
+    for i in range(int(N_solv)):
+        x = lx*np.random.rand()-lx/2.
+        y = ly*np.random.rand()-ly/2.
+        z = lz*np.random.rand()-lz/2.
+        grid_solv_pos.append([x,y,z])
+    return
+
+# an alternative wall builder
+def fcc_wall_builder():
+    # create an array of bead positions
+    xs = ys = np.linspace(-box_len/2.,box_len/2.,m.ceil(box_len/a))
+    zs = np.array([a/2.,0,-a/2.])#don't want overlaps
+    for z in zs:
+       for y in ys:
+            for x in xs:
+                # place the solvent particles
+                if z == 0:
+                    x_new = x+a/2.
+                    y_new = y+a/2.
+                    if x_new > box_len/2.:
+                        x_new = x_new - box_len
+                    if y_new > box_len/2.:
+                        y_new = y_new - box_len
+                    grid_wall_pos.append([x_new,y_new,z])
+                else:
+                    grid_wall_pos.append([x,y,z])
+    return
+
+#Ming function
+def atomlist(vrtList,tagnum):
+    Atom_list={}
+    for i in range(len(vrtList)):
+        Atom_list.setdefault(i+tagnum,[]).append(vrtList[i])
+        #Atom_index.append((vrtList[i]))
+    keys=Atom_list.keys()
+    return keys,Atom_list
+
+#Ming function
+def anglebonds(alist,r,modi): #generate angle bonds
+    bonds=[]
+    keys=list(alist.keys())
+    for i in range(len(alist)):
+        atomindex=[]
+        ii=i+modi
+        atom1=alist[ii]
+        for j in range(len(alist)):
+            jj=j+modi
+            atom2=alist[jj]
+            for k in range(3):
+                conditions=+(np.array(atom1[0][k])**2-np.array(atom2[0][k])**2)               
+            if  atom1 != atom2: 
+                if np.sqrt(abs(conditions)) < r:
+                    for q in range(len(alist)):
+                        qq=q+modi
+                        atom3=alist[qq]
+                        for k in range(3):
+                            conditions=+(np.array(atom2[0][k])**2-np.array(atom3[0][k])**2)
+                        if  atom2 != atom3:
+                            if atom1 != atom3: 
+                                if np.sqrt(abs(conditions)) < r:
+                                    atomindex=[keys[i],keys[j],keys[q]]
+                                    bonds.append(atomindex)
+                    
+    return bonds
+
+#Ming function
+def dihbonds(alist,r,modi): #generate angle bonds
+    bonds=[]
+    keys=list(alist.keys())
+    for i in range(len(alist)):
+        atomindex=[]
+        ii=i+modi
+        atom1=alist[ii]
+        for j in range(len(alist)):
+            jj=j+modi
+            atom2=alist[jj]
+            for k in range(3):
+                conditions=+(np.array(atom1[0][k])**2-np.array(atom2[0][k])**2)               
+            if  atom1 != atom2: 
+                if np.sqrt(abs(conditions)) < r:
+                    for p in range(len(alist)):
+                        pp=p+modi
+                        atom3=alist[pp]
+                        for k in range(3):
+                            conditions=+(np.array(atom2[0][k])**2-np.array(atom3[0][k])**2)
+                        if atom2 != atom3:
+                            if atom1 != atom3:
+                                if np.sqrt(abs(conditions)) < r:
+                                    for q in range(len(alist)):
+                                        qq=q+modi
+                                        atom4=alist[qq]
+                                        for k in range(3):
+                                            conditions=+(np.array(atom3[0][k])**2-np.array(atom4[0][k])**2)
+                                        if  atom1 != atom4:
+                                            if atom2 != atom4:
+                                                if atom3 != atom4: 
+                                                    if np.sqrt(abs(conditions)) < r:
+                                                        atomindex=[keys[i],keys[j],keys[p],keys[q]]
+                                                        bonds.append(atomindex)
+                                                        #print "atom=",i,j,p,q,"finish"
+                    
+    return bonds
+
+#Ming function
+def rm_repeat(alist):
+    alist.sort()
+    f_list=list(alist for alist,_ in itertools.groupby(alist))
+    return f_list
+
+# declare pos array(s)
+star_pos = []
+grid_solv_pos = []
+grid_wall_pos = []
+masses = []
+diameters = []
+bodies = []
+types = []
+bonds = []
+
+# call parser, which will fill pos and map_pos
+#parse = xml_parser()
+
+# create the particle grid
+solv_build = random_solv_plcr() #solvent
+wall_build = fcc_wall_builder()
+
+# ***TESTING***
+print('Length of solv_pos: ' + str(len(grid_solv_pos)))
+print('Length of wall_pos: ' + str(len(grid_wall_pos)))
+
+# aggreate all arrays
+full_pos = grid_solv_pos + grid_wall_pos
+
+# build a single list for each particle field
+masses.extend(int(N_solv)*[str(mass)]) #tack on the solvent
+masses.extend(len(grid_wall_pos)*[str(mass)]) #tack on the walls
+diameters.extend(int(N_solv)*['1.0']) #tack on the solvent
+diameters.extend(len(grid_wall_pos)*['1.0']) #tack on the walls
+bodies.extend(int(N_solv)*['-1']) #tack on the solvent
+bodies.extend(len(grid_wall_pos)*['-1']) #tack on the walls
+types_solv = int(N_solv)*['S1']
+types_wall = len(grid_wall_pos)*['wall']
+types = types_solv + types_wall
+
+#print('Print for testing')
+print(len(masses))
+print(len(diameters))
+print(len(bodies))
+print(len(types))
+print(len(bonds))
+print(len(full_pos))
+
+# catch improper configurations
+if len(full_pos) != len(masses):
+    print('The position and mass arrays are not the same length!')
+    sys.exit(-1)
+
+# write out the file
+with open('Init' + fName + '.xml', 'w') as inpFile:
+    inpFile.write('\n'.join(['<?xml version="1.1" encoding="UTF-8"?>',
+                             '<hoomd_xml version="1.5">',
+                             '<configuration time_step = "0" dimensions = "3">']))
+    inpFile.write('<box lx="{size1}" ly="{size2}" lz="{size3}" />\n'.format(size1=lx,size2=ly,size3=lz))
+    inpFile.write('<position>\n' + '\n'.join('{} {} {}'.format(x, y, z) for (x, y, z) in full_pos)
+        + '</position>\n')
+    inpFile.write('<mass>\n' + '\n'.join(str(mass) for mass in masses) + '</mass>\n')
+    inpFile.write('<diameter>\n' + '\n'.join(str(diameter) for diameter in diameters) + '</diameter>\n')
+    inpFile.write('<body>\n' + '\n'.join(str(body) for body in bodies) + '</body>\n')
+    inpFile.write('<type>\n' + '\n'.join(str(type) for type in types) + '</type>\n')
+    inpFile.write('\n</configuration>\n</hoomd_xml>\n')
+
+######################
+###### Main Run ######
+######################
+
+# read in the Init file
+system = init.read_xml(filename='Init' + fName + '.xml', wrap_coordinates = True)
+
+# NOTE: in these simulations epsilon has ben rescaled so that the ODT for the given volume fraction
+# is closer to that of the cores.
+dpd = pair.dpd(r_cut=1.0, T=1.)
+dpd.pair_coeff.set(system.particles.types, system.particles.types, A=20.0, gamma= 1.0)
+dpd.pair_coeff.set('wall', system.particles.types, A=0.0, gamma= 1.0)
+integrate.mode_standard(dt=0.01)
+
+# set up groups
+all = group.all()
+groupWALL = group.type(name='groupWALL', type='wall')
+notWALL=group.difference(name="particles-not-typeWALL", a=all, b=groupWALL)
+integrate.nve(group=notWALL)
+#integrate.nve(group=group.all())
+
+#make sure that the neighbor lists do not include particles that are bonded or are in the same body do not interact with one another
+nlist.reset_exclusions(exclusions = ['bond', 'body'])
+
+#start the logs, including restart file
+#dump.pos(filename="dump" + fName + ".pos", period=dump_period)
+#dump.dcd('traj-eql' + fName + '.dcd', period = dump_period, overwrite = True)
+
+# logs
+#analyze.log('energies-eql' + fName + '.txt', quantities=['temperature', 'potential_energy', 'kinetic_energy'], header_prefix='#', period = dump_period, overwrite = True)
+
+# run to set up geometry
+run(rsz_steps)
+
+# eql, release walls, eql
+run(eql_steps)
+
+
+######################
+#### Micelle Init ####
+######################
+
+# add the types via snapshot
+snap = system.take_snapshot(bonds=True)
+snap.bonds.types = ['tether', 'tether2']
+#system.restore_snapshot(snap, bonds=True)
+system.restore_snapshot(snap)
+
+# read the coordinates into a single flat list
+micelle = []
+f = open('micelle.txt', 'r')
+for line in f:
+    line = line.strip()
+    for number in line.split():
+        # check for small numbers, set to 0
+        if abs(float(number)) < 1e-14:
+            number = 0.0
+        micelle.append(float(number))
+
+# resphape the list into proper x, y, z coordinates
+micelle = [micelle[3*i : 3*(i+1)] for i in range(int(len(micelle)/3))]
+num_mbr_bds = len(micelle)
+
+# manipulate particle type(s)
+system.particles.types.add('micelle')
+for m in range(len(N_cell*micelle)):
+    system.particles.add('micelle')
+
+# add the particles, shift the positions
+# drops are stacked along the z-axis, upper quadrant
+cnt = 0
+mcl_z_arr_idx = 0
+mcl_z_arr = np.linspace(-1*system.box.Lz/4.75,system.box.Lz/4.75,N_cell)
+for p in system.particles:
+    if p.type=='micelle':
+        if cnt % num_mbr_bds == 0 and cnt > 0:
+            mcl_z_arr_idx += 1
+        x_new = micelle[cnt % len(micelle)][0] + system.box.Lx/4.75
+        y_new = micelle[cnt % len(micelle)][1] + system.box.Ly/4.75
+        z_new = micelle[cnt % len(micelle)][2] + mcl_z_arr[mcl_z_arr_idx]
+        p.position = (x_new, y_new, z_new)
+        p.diameter = 1.0
+        cnt += 1
+
+# Set up lists to identify which particles should be bonded
+# Calculate the distance from p_i to all other particles, p_j
+# If this distance is ~particle diameter, then add a bond
+# NOTE: Will need to keep two lists of tags
+cnt = 0
+r_max = 0.75
+tags_list = []
+tags_mcls = []
+groupM = group.type(name="micelle-particles", type='micelle')
+for p in groupM:
+    tags = []
+    tags_mcls.append(p.tag)
+    p_i = p.position
+    for p in groupM:
+        p_j = p.position
+        dx = p_j[0] - p_i[0]
+        dy = p_j[1] - p_i[1]
+        dz = p_j[2] - p_i[2]
+        r = (dx**2 + dy**2 + dz**2)**(1./2.)
+        if r > 0.1 and r < r_max:
+            tags.append(p.tag)
+    tags_list.append(tags)
+
+# loop through lists, add the bonds
+cnt = 0
+for t_i in tags_mcls:
+    for t_j in tags_list[cnt]:
+        if t_i == t_j:
+            pass
+        else:
+            system.bonds.add("tether", t_i, t_j)
+    cnt += 1
+
+# Ming code for angle and dihedral bonds
+atomindex=tags_mcls[0]
+(keys,alist)=atomlist(micelle,atomindex)
+Anglebond=rm_repeat(anglebonds(alist,r_max,atomindex))
+Dihbond=rm_repeat(dihbonds(alist,r_max,atomindex))
+for i in range(len(Anglebond)):
+    system.angles.add("Hb",Anglebond[i][0],Anglebond[i][1],Anglebond[i][2])
+for k in range(len(Dihbond)):
+    system.dihedrals.add("Db",Dihbond[k][0],Dihbond[k][1],Dihbond[k][2],Dihbond[k][3])
+
+# dump the final xml
+dump.xml(filename='Init' +  fName  + '.xml', all=True)
