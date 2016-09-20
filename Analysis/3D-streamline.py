f0c95556f13e05786aef0243e3f82b67c5068ee3
huanghun <huanghun@umich.edu>
2016-09-20 00:06:59 -0400

    Initial commit

--- Analysis/3D-streamline.py
+++ Analysis/3D-streamline.py
@@ -20,15 +20,18 @@ def readData(id):
     vList = list(g.recordsNamed('velocity'))[endFr-1][1]
     # grab velocity by type, ignore walls
     v_S1 = vList[types == 0]
-    #v_S2 = vList[types == 1]
+    v_S2 = vList[types == 1]
     # grab position by type, ignore walls
     p_S1 = pList[types == 0]
-    #p_S2 = pList[types == 1]
+    p_S2 = pList[types == 1]
     v=map(list, zip(*v_S1)) #transpose the list
+    v1=map(list,zip(*v_S2))
     p=map(list, zip(*p_S1))
-    out = [p[2],v[0]]
+    p1=map(list,zip(*p_S2))
+    #out = [p[2],v[0]]
+    out = [p1[0],p1[1],p1[2],v1[0],v1[1],v1[2]]
     out = map(list, zip(*out))
-    out1 = [p[0],p[2],v[0],v[2]] #0 stands for x direction, 2 stands for z direction
+    out1 = [p[0],p[1],p[2],v[0],v[1],v[2]] #0 stands for x direction, 2 stands for z direction
     out1 = map(list, zip(*out1))
     #index, value = max(enumerate(out[2]), key=operator.itemgetter(1))
     #v_max=[out[1][index],out[2][index]]
@@ -63,17 +66,23 @@ def axis(data): #fix BC
             aa=aa+20
         bb=data[z][1]
         if bb<0:
-            bb=bb+40
+            bb=bb+20
         cc=data[z][2]
+        if cc<0:
+            cc=cc+40
         dd=data[z][3]
-        out.append([aa,bb,cc,dd])
+        ee=data[z][4]
+        ff=data[z][5]
+        out.append([aa,bb,cc,dd,ee,ff])
 #print out
     return out
 def avg(data): #average position and velocity for each grid
     #out=[]
     px=[]
+    py=[]
     pz=[]
     vx=[]
+    vy=[]
     vz=[]
     for z in range(len(data)): #collect all x or z or vx or vz in Beads
         aa=data[z][0]
@@ -81,39 +90,55 @@ def avg(data): #average position and velocity for each grid
             aa=aa+20
         bb=data[z][1]
         if bb<0:
-            bb=bb+40
+            bb=bb+20
         cc=data[z][2]
+        if cc<0:
+            cc=cc+40
         dd=data[z][3]
+        ee=data[z][4]
+        ff=data[z][5]
         px.append(aa)
-        pz.append(bb)
-        vx.append(cc)
-        vz.append(dd)
+        py.append(bb)
+        pz.append(cc)
+        vx.append(dd)
+        vy.append(ee)
+        vz.append(ff)
         global average_px
+        global average_py
         global average_pz
         global average_vx
+        global average_vy
         global average_vz
         average_px=np.mean(px)
+        average_py=np.mean(py)
         average_pz=np.mean(pz)
         average_vx=np.mean(vx)
+        average_vy=np.mean(vy)
         average_vz=np.mean(vz)
 #out.append([average_px,average_pz,average_vx,average_vz])
-    return average_px,average_pz,average_vx,average_vz
-def sampleDistances_xz(nice_level,data):
+    return average_px,average_py,average_pz,average_vx,average_vy,average_vz
+def sampleDistances_xz(nice_level_x,nice_level_y,nice_level_z,data):
     data=axis(data)
     data_trans=zip(*data)
     x=np.array(data_trans[0])#transform data matrix, x direction
-    x1=np.array(data_trans[1])#z position
+    y=np.array(data_trans[1])#y position
+    z=np.array(data_trans[2])
     #x2=np.array(data_trans)[2])#y position
-    y=np.array(data_trans[2])#vx
-    y1=np.array(data_trans[3])#vz
+    vx=np.array(data_trans[3])#vx
+    vy=np.array(data_trans[4])#vy
+    vz=np.array(data_trans[5])
     beg=min(x) #x direction
-    beg1=min(x1) #z direction
+    beg1=min(y) #y direction
+    beg2=min(z) #z direction
     end=max(x)
-    end1=max(x1)
+    end1=max(y)
+    end2=max(z)
     start=beg
     start1=beg1
-    interval=(end-beg)/nice_level
-    interval1=(end1-beg1)/nice_level
+    start2=beg
+    interval=(end-beg)/(nice_level_x) #x axis
+    interval1=(end1-beg1)/(nice_level_y) #y axis
+    interval2=(end2-beg2)/nice_level_z #z axis
     #Judge where S2 coordinate along y direction
     a={} #position
     b={} #velocity
@@ -121,31 +146,39 @@ def sampleDistances_xz(nice_level,data):
     out=[]
     print "interval",interval
     print "interval1",interval1
-    print "beg",beg
-    print "end",end
-    print "beg1",beg1
-    print "end1",end1
-    for i in range(0,nice_level,1):#collect beads in grid of x direction
-        Beads_org=[]
+    print "interval2",interval2
+    print "beg",beg,"end",end
+    print "beg1",beg1,"end1",end1
+    print "beg2",beg2,"end2",end2
+    for i in range(0,nice_level_x,1):#collect beads in grid of x direction
+        Beads_x=[]
         bin=beg+interval*(i+1)#along x direction
         for ii in range(len(data)):
             print "start=",start,"bin=",bin
             if data[ii][0] > start and data[ii][0] < bin:
-                Beads_org.append(data[ii])
-        print Beads_org
-        for iii in range(0,nice_level,1): #collect beads in grid of z direction
-            Beads=[]
-            bin1=beg1+interval1*(iii+1)#along z direction
-            for iv in range(len(Beads_org)): #put fit balls inside the bin
+                Beads_x.append(data[ii])
+        for iii in range(0,nice_level_y,1): #collect beads in grid of y direction
+            Beads_y=[]
+            bin1=beg1+interval1*(iii+1)
+            for iv in range(len(Beads_x)): #put fit balls inside the bin
                 print "start1=",start1,"bin1=",bin1
-                if Beads_org[iv][1] > start1 and Beads_org[iv][1] < bin1:
-                    Beads.append(Beads_org[iv])
-                print "round=",iv
+                if Beads_x[iv][1] > start1 and Beads_x[iv][1] < bin1:
+                    Beads_y.append(Beads_x[iv])
+            for v in range(0,nice_level_z,1):
+                Beads_z=[]
+                bin2=beg2+interval2*(v+1)
+                for vi in range(len(Beads_y)): #collect beads in grid of z direction
+                    print "round=",v,vi
+                    if Beads_y[vi][2] > start2 and Beads_y[vi][2] < bin2:
+                        Beads_z.append(Beads_y[vi])
+                start2 = bin2
         #print Beads
-            print "Average collect beads"
-            [p_x,p_z,v_x,v_z]=avg(Beads)
-            print "put into matrix"
-            out.append([p_x,p_z,v_x,v_z])
+                print "Average collect beads"
+                if not Beads_z:
+                    continue
+                [p_x,p_y,p_z,v_x,v_y,v_z]=avg(Beads_z)
+                print "put into matrix"
+                out.append([p_x,p_y,p_z,v_x,v_y,v_z])
             print out
             start1 = bin1
         start = bin
@@ -153,16 +186,16 @@ def sampleDistances_xz(nice_level,data):
 id = sys.argv[1]
 [data,data1] = readData(id)
 print len(data)
-#a=sampleDistances(500,data)
-b=sampleDistances_xz(25,data1)
-f = file("1-plot-data.txt","w")
+a=sampleDistances_xz(25,25,25,data) #droplet
+b=sampleDistances_xz(10,50,15,data1)#fluid
+f = file("fluid-plot-data.txt","w")
 for r in b:
     f.write("\t".join(map(str,r)) + "\n")
 f.close()
-f = file("plot-data.txt","w")
-#for r in a:
-#    f.write("\t".join(map(str,r)) + "\n")
-#f.close()
+f = file("droplet-plot-data.txt","w")
+for r in a:
+    f.write("\t".join(map(str,r)) + "\n")
+f.close()
 #v_average=open("vFile" + id + ".txt","w")
 #v_average.write("Position= "+ str(v_max[0])+'\n')
 #v_average.write("Max-velocity= "+ str(v_max[1])+'\n')
